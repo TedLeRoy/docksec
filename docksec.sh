7b2ae5620937fa8fe1a276d475d75dcd58148cce
Ted LeRoy <ted.leroy@protonmail.com>
2018-04-03 13:22:02 -0400

    Create docksec.sh
    Initial commit of docksec.sh

---
+++ docksec.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# bash script to secure Docker Hosts. Written on 16.04 LTS Server
+# Written by Ted LeRoy with help from Google and the Linux community
+# Follow or contribute on GitHub here:
+# https://github.com/TedLeRoy/docksec
+
+# Defining Colors for text output
+red=$( tput setaf 1 );
+yellow=$( tput setaf 3 );
+green=$( tput setaf 2 );
+normal=$( tput sgr 0 );
+
+# Defining Header
+HEADER="
+ubuntu-update.sh Copyright (C) 2018 Ted LeRoy
+Script to remediate findings of Docker Bench for Security. Harden your Docker host.
+This program comes with ABSOLUTELY NO WARRANTY see
+https://github.com/TedLeRoy/docksec/blob/master/LICENSE.md
+This is free software, and you are welcome to redistribute it
+under certain conditions.
+See  https://github.com/TedLeRoy/docksec/blob/master/LICENSE.md
+for details."
+
+echo ""
+echo "#Setting auditd to monitor docker files
+-w /lib/systemd/system/docker.service -k docker
+-w /lib/systemd/system/docker.socket -k docker
+-w /usr/bin/docker -p rwxa -k docker
+-w /var/lib/docker/ -p rwxa -k docker
+-w /etc/docker/ -p rwxa -k docker
+-w /etc/default/docker -p rwxa -k docker
+-w /etc/docker/daemon.json -p rwxa -k do1cker
+-w /usr/bin/docker-containerd -p rwxa -k docker
+-w /usr/bin/docker-runc -p rwxa -k docker
+" >> /etc/audit/audit.rules
+echo ""
+echo "auditd set"
+echo ""
+echo "Restarting auditd service"
+service auditd restart
+echo ""
+echo "auditd service restarted"
+echo ""
+echo "Restricting network traffic between containers"
+#service docker stop
+#dockerd --icc=false
+#service docker start
+echo ""
+echo "Network traffic between containers restricted"
+echo ""
